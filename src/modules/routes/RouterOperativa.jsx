df234300976805a645df49ccace848b9397eadbf
Juan Silupu <juansilupumaza@gmail.com>
2021-04-30 11:20:28 -0500

    feat: add suspense and lazy

--- src/modules/routes/RouterOperativa.jsx
+++ src/modules/routes/RouterOperativa.jsx
@@ -1,21 +1,29 @@
-import React, { useEffect, useState } from 'react';
+import React, { Suspense, lazy } from 'react';
 
 //react router
 import { Route, Switch, Redirect } from 'react-router-dom';
 
 //Routes
-import { AuthRoutes, AuthRouter } from '../auth';
-import { HomeRoutes, HomeRouter } from '../home';
-import { DashboardRoutes, DashboardRouter } from '../dashboard';
-import { AdminRoutes, AdminRouter } from '../admin';
+import { AuthRoutes,  } from '../auth';
+import { HomeRoutes,  } from '../home';
+import { DashboardRoutes,  } from '../dashboard';
+import { AdminRoutes,  } from '../admin';
+import {ApplicantRoutes} from '../applicant'
 import { SessionRoutes } from '../shared/libs/sessionRoutes';
-import {ApplicantRouter,ApplicantRoutes} from '../applicant'
+import Error from "../home/pages/Error";
+
+//React.lazy
+const HomeRouter = lazy(() => import("../home/routes/HomeRouter"))
+const AuthRouter = lazy(() => import("../auth/routes/AuthRouter"))
+const DashboardRouter = lazy(() => import("../dashboard/routes/DashboardRouter"))
+const AdminRouter = lazy(() => import("../admin/routes/AdminRouter"))
+const ApplicantRouter = lazy(() => import("../applicant/routes/ApplicantRouter"))
+
 
 //Sidebar
-import { Navigation } from "../shared/components";
+// import { Navigation } from "../shared/components";
 
 //404
-import Error from "../home/pages/Error";
 
 const RouterOperativa = () => {
     // Get session and user
@@ -24,16 +32,18 @@ const RouterOperativa = () => {
     return (
         <>
             <Switch>
-                <Route exact path={HomeRoutes} component={HomeRouter} />
-                <Route exact path={AuthRoutes} component={AuthRouter} />
-                <Route exact path={DashboardRoutes} component={DashboardRouter} />
-                <Route exact path={AdminRoutes} component={AdminRouter} />
-                <Route exact path={ApplicantRoutes} component={ApplicantRouter} />
-                {
-                    initRoute !== "/" ?
-                    <Redirect to={initRoute} /> :
-                    <Route path="*" component={Error} />
-                }
+                <Suspense fallback={<div>Loading...</div>}>
+                    <Route exact path={HomeRoutes} component={HomeRouter} />
+                    <Route exact path={AuthRoutes} component={AuthRouter} />
+                    <Route exact path={DashboardRoutes} component={DashboardRouter} />
+                    <Route exact path={AdminRoutes} component={AdminRouter} />
+                    <Route exact path={ApplicantRoutes} component={ApplicantRouter} />
+                    {
+                        initRoute !== "/" ?
+                        <Redirect to={initRoute} /> :
+                        <Route path="*" component={Error} />
+                    }
+                </Suspense>
                 
             </Switch>
         </>
