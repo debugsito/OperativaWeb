81964991d41653b9c09d9107503a787168d22fb4
Juan Silupu <juansilupumaza@gmail.com>
2021-05-31 17:24:06 -0500

    feat(download): download excel

---
+++ src/modules/shared/libs/api/service_dowload.api.js
@@ -0,0 +1,37 @@
+import axios from "axios";
+import AppSession from "../session/AppSession";
+
+const api = axios.create({
+  baseURL: process.env.REACT_APP_API_URL,
+  // baseURL: "http://localhost:3005/api/",
+  headers: {
+    "Access-Control-Allow-Origin": "*",
+    "Content-Type": "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet",
+    "Accept": "application/json",
+  },
+});
+
+api.interceptors.request.use(async (config) => {
+  const newConfig = { ...config };
+  const token = AppSession.get();
+  if (token) {
+    newConfig.headers.Authorization = `${token}`;
+  }
+  return newConfig;
+});
+
+api.interceptors.response.use(
+  async (response) => {
+    if (response?.data?.token) {
+      AppSession.create(response.data.token)
+    }
+    return response;
+  },
+  async (error) => {
+    if (error.response.status === 401) {
+    }
+    return Promise.reject(error);
+  }
+);
+
+export default api;
--- src/store/services/admin/user.service.js
+++ src/store/services/admin/user.service.js
@@ -1,4 +1,5 @@
 import api from "../../../modules/shared/libs/api";
+import api_for_download from "../../../modules/shared/libs/api/service_dowload.api";
 
 const getAccounts = async () => {
     const response = await api.get('/accounts');
@@ -19,10 +20,15 @@ const getReport = async (params) => {
     const response = await api.get(`/account/users/report/${params.startDate}/${params.finishDate}`);
     return response;
 }
+const getFileReport = async (params) => {
+    const response = await api_for_download.get(`/account/users/report/${params.startDate}/${params.finishDate}/export`);
+    return response;
+}
 
 export default {
     getAccounts,
     acceptAccount,
     denyAccount,
-    getReport
+    getReport,
+    getFileReport
 }
