3a1278710e3fad7f193fa45f0be38e6a5f3aef3d
Marco Lopez Vega <milo@Marcos-MacBook-Pro.local>
2021-12-17 18:14:45 -0500

    agregando etiqueta de activo a la navegacion

--- src/modules/dashboard/components/TabsAssignEvaluations/index.js
+++ src/modules/dashboard/components/TabsAssignEvaluations/index.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { AppBar, Tabs, Tab, makeStyles } from '@material-ui/core';
+import { AppBar, Tabs, Tab, makeStyles } from "@material-ui/core";
 import { TabPanel } from "../../../shared/components";
 import TabEvaluation from "./TabEvaluation";
 import TabVerificativa from "./TabVerificativa";
@@ -8,81 +8,101 @@ import TabEvaluativa from "./TabEvaluativa";
 import TabEntrevista from "./TabEntrevista";
 
 const useStyles = makeStyles((theme) => ({
-    root: {
-        flexGrow: 1,
-        backgroundColor: theme.palette.background.paper,
+  root: {
+    flexGrow: 1,
+    backgroundColor: theme.palette.background.paper,
+  },
+  rootTabs: {
+    background: "#E7EAF1",
+  },
+  rootTab: {
+    "&$selected": {
+      color: "var(--paragraphColor)",
+      background: "#fff",
     },
-    rootTabs: {
-        background: "#E7EAF1",
-    },
-    rootTab: {
-        '&$selected': {
-            color: "var(--paragraphColor)",
-            background: "#fff",
-        }
-    },
-    selected: {},
+  },
+  selected: {},
 }));
 
 export default function ApplicantsTabs(props) {
-    const classes = useStyles();
-    const [value, setValue] = React.useState(0);
+  const classes = useStyles();
+  const [value, setValue] = React.useState(0);
 
-    const handleChange = (event, newValue) => {
-        setValue(newValue);
-    };
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
+  };
 
-    const nextTab = () => {
-        setValue(currentValue => currentValue + 1);
-    }
+  const nextTab = () => {
+    setValue((currentValue) => currentValue + 1);
+  };
 
-    const backTab = () => {
-        setValue(currentValue => currentValue - 1);
-    }
+  const backTab = () => {
+    setValue((currentValue) => currentValue - 1);
+  };
 
-    return (
-        //   <div className={classes.root}>
-        <>
-            <AppBar position="static">
-                <Tabs
-                    value={value}
-                    onChange={handleChange}
-                    aria-label="applicants tabs"
-                    indicatorColor="primary"
-                    variant="fullWidth"
-                    textColor="primary"
-                    classes={{ root: classes.rootTabs }}
-                >
-                    <Tab classes={{ root: classes.rootTab, selected: classes.selected }} label="Preguntas" {...a11yProps(0)} />
-                    <Tab classes={{ root: classes.rootTab, selected: classes.selected }} label="Verificativa" {...a11yProps(1)} />
-                    <Tab classes={{ root: classes.rootTab, selected: classes.selected }} label="Médico" {...a11yProps(2)} />
-                    <Tab classes={{ root: classes.rootTab, selected: classes.selected }} label="Evaluativa" {...a11yProps(3)} />
-                    <Tab classes={{ root: classes.rootTab, selected: classes.selected }} label="Entrevista" {...a11yProps(4)} />
-                </Tabs>
-            </AppBar>
-            <TabPanel value={value} index={0}>
-                <TabEvaluation nextTab={nextTab} />
-            </TabPanel>
-            <TabPanel value={value} index={1}>
-                <TabVerificativa nextTab={nextTab} backTab={backTab}/>
-            </TabPanel>
-            <TabPanel value={value} index={2}>
-                <TabMedico nextTab={nextTab} backTab={backTab}/>
-            </TabPanel>
-            <TabPanel value={value} index={3}>
-                <TabEvaluativa nextTab={nextTab} backTab={backTab}/>
-            </TabPanel>
-            <TabPanel value={value} index={4}>
-                <TabEntrevista backTab={backTab}/>
-            </TabPanel>
-        </>
-        //   </div>
-    );
+  return (
+    //   <div className={classes.root}>
+    <>
+      <AppBar position="static">
+        <Tabs
+          value={value}
+          onChange={handleChange}
+          aria-label="applicants tabs"
+          indicatorColor="primary"
+          variant="fullWidth"
+          textColor="primary"
+          classes={{ root: classes.rootTabs }}
+        >
+          <Tab
+            classes={{ root: classes.rootTab, selected: classes.selected }}
+            label="Preguntas"
+            {...a11yProps(0)}
+          />
+          <Tab
+            classes={{ root: classes.rootTab, selected: classes.selected }}
+            label="Verificativa"
+            {...a11yProps(1)}
+          />
+          <Tab
+            classes={{ root: classes.rootTab, selected: classes.selected }}
+            label="Médico"
+            {...a11yProps(2)}
+          />
+          <Tab
+            classes={{ root: classes.rootTab, selected: classes.selected }}
+            label="Evaluativa"
+            {...a11yProps(3)}
+          />
+          <Tab
+            classes={{ root: classes.rootTab, selected: classes.selected }}
+            label="Entrevista"
+            {...a11yProps(4)}
+          />
+        </Tabs>
+      </AppBar>
+      <TabPanel value={value} index={0}>
+        <TabEvaluation nextTab={nextTab} />
+      </TabPanel>
+      <TabPanel value={value} index={1}>
+        <TabVerificativa nextTab={nextTab} backTab={backTab} />
+      </TabPanel>
+      <TabPanel value={value} index={2}>
+        <TabMedico nextTab={nextTab} backTab={backTab} />
+      </TabPanel>
+      <TabPanel value={value} index={3}>
+        <TabEvaluativa nextTab={nextTab} backTab={backTab} />
+      </TabPanel>
+      <TabPanel value={value} index={4}>
+        <TabEntrevista backTab={backTab} />
+      </TabPanel>
+    </>
+    //   </div>
+  );
 }
 
 function a11yProps(index) {
-    return {
-        id: `simple-tab-${index}`,
-        'aria-controls': `simple-tabpanel-${index}`,
-    };
+  return {
+    id: `simple-tab-${index}`,
+    "aria-controls": `simple-tabpanel-${index}`,
+  };
 }
