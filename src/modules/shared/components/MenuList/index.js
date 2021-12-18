bbd00f7ccf4d730c3280dd4939c74b4c73aad2ae
Juan Silupu <juansilupumaza@gmail.com>
2021-12-17 19:04:45 -0500

    fix: borde de menu tipo contexto

--- src/modules/shared/components/MenuList/index.js
+++ src/modules/shared/components/MenuList/index.js
@@ -1,11 +1,20 @@
 import React from 'react'
 import Menu from "@material-ui/core/Menu"
+import { makeStyles } from '@material-ui/styles'
+
+const useStyles = makeStyles(theme => ({
+    paper:{
+
+    }
+}))
 
 export default function Index({ children, anchorEl, handleClose }) {
-
+    const classes = useStyles
 
     return (
         <Menu
+            elevation={1}
+            classes={classes}
             getContentAnchorEl={null}
             id="simple-menu"
             anchorEl={anchorEl}
