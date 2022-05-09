21dc3fe354ca70f16fe2ca867bf2d67a0ee778ec
Marlon Laguna <anthonylj.cr250@gmail.com>
2022-05-09 18:30:37 -0500

    WIP

--- src/App.js
+++ src/App.js
@@ -1,30 +1,57 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './assets/css/variables.css';
 import './assets/css/footerTable.css';
 import './index.css';
+import logoOperativa from './assets/images/operativa192.png';
 
 import AppRouter from './routers';
 
 function App() {
+    const [installPrompt, setInstallPrompt] = useState(null)
+    const [installButton, setInstallButton] = useState(false)
 
-    window.addEventListener('beforeinstallprompt',e=>{
+    window.addEventListener('beforeinstallprompt', e => {
         // For older browsers
         e.preventDefault();
         console.log("Install Prompt fired");
-        alert('Entré al flujo pwa');
-        this.installPrompt = e;
+        setInstallPrompt(e);
         // See if the app is already installed, in that case, do nothing
-        if((window.matchMedia && window.matchMedia('(display-mode: standalone)').matches) || window.navigator.standalone === true){
+        if ((window.matchMedia && window.matchMedia('(display-mode: standalone)').matches) || window.navigator.standalone === true) {
             return false;
         }
         // Set the state variable to make button visible
-        /*this.setState({
-            installButton:true
-        })*/
-    })
+        if ((window.innerWidth <= 800) && (window.innerHeight <= 600)) {
+            setInstallButton(true);
+        }
+    });
+
+    const installApp = async () => {
+        if (!installPrompt) return false;
+        installPrompt.prompt();
+        let outcome = await installPrompt.userChoice;
+        if (outcome.outcome == 'accepted') {
+            console.log("App Installed")
+        } else {
+            console.log("App not installed");
+        }
+        // Remove the event reference
+        setInstallPrompt(null);
+        // Hide the button
+        setInstallButton(false);
+    }
 
     return (
-        <AppRouter/>
+        <>
+            {installButton &&
+                <>
+                    <div className="alert-install-pwa" onClick={installApp}>
+                        <img src={logoOperativa} alt="Operativa"/>
+                        <span> Agrega Operativa a la pantalla principal</span>
+                    </div>
+                </>
+            }
+            <AppRouter/>
+        </>
     );
 }
 
